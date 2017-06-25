7b9a3e8476f7931e5117998045cee1f913bac468
Robert Friberg <robert@devrex.se>
2017-06-25 19:22:22 +0200

    extracted duplicate subscription type

--- Memstate.Core/FileCommandStore.cs
+++ Memstate.Core/FileCommandStore.cs
@@ -7,37 +7,7 @@ namespace Memstate.Core
 {
     public class FileCommandStore : IHandle<Command>, ICommandSubscriptionSource
     {
-        class Subscription : IHandle<JournalRecord>, ICommandSubscription
-        {
-            public readonly Guid Id = Guid.NewGuid();
-            private readonly Action<JournalRecord> _callback;
-            private long _nextRecord;
-            private readonly Action<Subscription> _onDisposed;
-
-            public Subscription(Action<JournalRecord> callback, long nextRecord, Action<Subscription> onDisposed)
-            {
-                _nextRecord = nextRecord;
-                _callback = callback;
-                _onDisposed = onDisposed;
-            }
-
-            public void Dispose()
-            {
-                _onDisposed.Invoke(this);
-            }
-
-            public bool Ready()
-            {
-                return true;
-            }
-
-            public void Handle(JournalRecord record)
-            {
-                if (record.RecordNumber != _nextRecord) throw new InvalidOperationException("expected version " + _nextRecord + ", got " + record.RecordNumber);
-                _nextRecord++;
-                _callback.Invoke(record);
-            }
-        }
+        
 
         private readonly Dictionary<Guid, Subscription> _subscriptions;
         private readonly Batcher<Command> _commandBatcher;
---
+++ Memstate.Core/Subscription.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Memstate.Core
+{
+    public class Subscription : IHandle<JournalRecord>, ICommandSubscription
+    {
+        public readonly Guid Id = Guid.NewGuid();
+        private readonly Action<JournalRecord> _callback;
+        private long _nextRecord;
+        private readonly Action<Subscription> _onDisposed;
+
+        public Subscription(Action<JournalRecord> callback, long nextRecord, Action<Subscription> onDisposed)
+        {
+            _nextRecord = nextRecord;
+            _callback = callback;
+            _onDisposed = onDisposed;
+        }
+
+        public void Dispose()
+        {
+            _onDisposed.Invoke(this);
+        }
+
+        public bool Ready()
+        {
+            return true;
+        }
+
+        public void Handle(JournalRecord record)
+        {
+            if (record.RecordNumber != _nextRecord) throw new InvalidOperationException("expected version " + _nextRecord + ", got " + record.RecordNumber);
+            _nextRecord++;
+            _callback.Invoke(record);
+        }
+    }
+}

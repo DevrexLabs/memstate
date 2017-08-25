4efd15d18da75fed58d7302af707a3017571c0f3
Robert Friberg <robert@devrex.se>
2017-08-25 10:21:09 +0200

    refactored engine tests

--- Memstate.Tests/EngineTests.cs
+++ Memstate.Tests/EngineTests.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using FakeItEasy;
 using Xunit;
 
@@ -11,40 +8,48 @@ namespace Memstate.Tests
     {
         private readonly IJournalSubscriptionSource _fakeSubscriptionSource;
         private readonly IJournalSubscription _fakeSubscription;
-
+        private readonly IJournalWriter _fakeJournalWriter;
+        private readonly Engine<Object> _engine;
+        private readonly int _nextRecordNumber;
+        
         public EngineTests()
         {
             _fakeSubscriptionSource = A.Fake<IJournalSubscriptionSource>();
             _fakeSubscription = A.Fake<IJournalSubscription>();
-        }
+            _fakeJournalWriter = A.Fake<IJournalWriter>();
 
-        private static IEnumerable<object[]> RecordNumbers()
-        {
-            return new [] {0, 1, 3, 3, 4, 5}
-                .Select(i => new object[] {i});
-        }
+            _nextRecordNumber = DateTime.Now.Millisecond;
 
-        [Theory, MemberData(nameof(RecordNumbers))]
-        public void Constructor_subscribes_to_journal_records_from_correct_recordNumber(int nextRecordNumber)
-        {
+            A.CallTo(() => _fakeSubscriptionSource.Subscribe(_nextRecordNumber, A<Action<JournalRecord>>._))
+                .Returns(_fakeSubscription);
+
             var config = new Config();
-            var engine  = new Engine<Object>(config, new Object(), _fakeSubscriptionSource, A.Fake<IJournalWriter>(), nextRecordNumber);
-            A.CallTo(() => _fakeSubscriptionSource.Subscribe(nextRecordNumber, A<Action<JournalRecord>>._))
+            _engine = new Engine<Object>(config, new Object(), _fakeSubscriptionSource, _fakeJournalWriter, _nextRecordNumber);
+        }
+
+
+        [Fact]
+        public void Constructor_subscribes_to_journal_records_from_correct_recordNumber()
+        {
+            A.CallTo(() => _fakeSubscriptionSource.Subscribe(_nextRecordNumber, A<Action<JournalRecord>>._))
                 .MustHaveHappened(Repeated.Exactly.Once);
         }
 
         [Fact]
         public void Subscription_is_disposed_when_engine_is_disposed()
         {
-            int nextRecordNumber = 0;
-            A.CallTo(() => _fakeSubscriptionSource.Subscribe(nextRecordNumber, A<Action<JournalRecord>>._))
-                .Returns(_fakeSubscription);
+            _engine.Dispose();
 
-            var config = new Config();
-            var engine = new Engine<Object>(config, new Object(), _fakeSubscriptionSource, A.Fake<IJournalWriter>(), nextRecordNumber);
-            engine.Dispose();
+            A.CallTo(() => _fakeSubscription.Dispose())
+                .MustHaveHappened(Repeated.Exactly.Once);
+        }
 
-            A.CallTo(() => _fakeSubscription.Dispose()).MustHaveHappened(Repeated.Exactly.Once);
+        [Fact]
+        public void Writer_is_disposed_when_engine_is_disposed()
+        {
+            _engine.Dispose();
+            A.CallTo(() => _fakeJournalWriter.Dispose())
+                .MustHaveHappened(Repeated.Exactly.Once);
         }
 
     }

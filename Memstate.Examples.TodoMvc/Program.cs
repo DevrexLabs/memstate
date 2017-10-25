d17abfbe37bed0c5457c315ce8c5fefb66c16a2a
Kim Johansson <kim@kumiko.se>
2017-10-25 16:04:07 +0200

    Make class static
    No need to be able to instantiate the class.

--- Memstate.Examples.TodoMvc/Program.cs
+++ Memstate.Examples.TodoMvc/Program.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Memstate.Examples.TodoMvc
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var host = new WebHostBuilder()
                 .UseKestrel()

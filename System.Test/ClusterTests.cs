04c3ef01248d3330793e32cea91abd9324cdec1c
Kim Johansson <kim@kumiko.se>
2017-11-06 13:49:36 +0100

    Test cluster functionality

---
+++ System.Test/ClusterTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Memstate;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.Test
+{
+    public class ClusterTests
+    {
+        private readonly ITestOutputHelper _log;
+        private readonly string _randomStreamName;
+
+        public ClusterTests(ITestOutputHelper log)
+        {
+            _log = log;
+            _randomStreamName = "memstate" + Guid.NewGuid().ToString("N").Substring(0, 10);
+        }
+
+        // One writer, multiple readers
+        [Theory]
+        [ClassData(typeof(TestConfigurations))]
+        public async Task CanWriteOneAndReadFromMany(MemstateSettings settings)
+        {
+            const int records = 100;
+
+            settings.LoggerFactory.AddProvider(new TestOutputLoggingProvider(_log));
+            settings.StreamName = _randomStreamName;
+
+            var writer = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+
+            var readers = new Engine<List<string>>[3];
+
+            readers[0] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+            readers[1] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+            readers[2] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+
+            foreach (var number in Enumerable.Range(1, records))
+            {
+                var command = new AddStringCommand($"{number}");
+                var count = await writer.ExecuteAsync(command).ConfigureAwait(false);
+                Assert.Equal(number, count);
+            }
+
+            await writer.DisposeAsync().ConfigureAwait(false);
+
+            foreach (var reader in readers)
+            {
+                var strings = await reader.ExecuteAsync(new GetStringsQuery()).ConfigureAwait(false);
+
+                Assert.Equal(records, strings.Count);
+
+                await reader.DisposeAsync().ConfigureAwait(false);
+            }
+        }
+
+        // Multiple writers, one reader
+        [Theory]
+        [ClassData(typeof(TestConfigurations))]
+        public async Task CanWriteManyAndReadFromOne(MemstateSettings settings)
+        {
+            const int records = 100;
+
+            settings.LoggerFactory.AddProvider(new TestOutputLoggingProvider(_log));
+            settings.StreamName = _randomStreamName;
+
+            var reader = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+
+            var writers = new Engine<List<string>>[3];
+
+            writers[0] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+            writers[1] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+            writers[2] = await Engine.StartAsync<List<string>>(settings).ConfigureAwait(false);
+
+            var totalCount = 0;
+            
+            for (var i = 0; i < writers.Length; i++)
+            {
+                var writer = writers[i];
+
+                foreach (var number in Enumerable.Range(1, records))
+                {
+                    var command = new AddStringCommand($"{number}");
+                    var count = await writer.ExecuteAsync(command).ConfigureAwait(false);
+                    Assert.Equal(++totalCount, count);
+                }
+
+                await writer.DisposeAsync().ConfigureAwait(false);
+            }
+
+            var strings = await reader.ExecuteAsync(new GetStringsQuery()).ConfigureAwait(false);
+
+            Assert.Equal(records * writers.Length, strings.Count);
+
+            await reader.DisposeAsync().ConfigureAwait(false);
+        }
+
+        // Multiple writers, multiple readers
+        [Theory]
+        [ClassData(typeof(TestConfigurations))]
+        public async Task CanWriteManyAndReadFromMany(MemstateSettings settings)
+        {
+        }
+    }
+}

3c07ad1f845d688bdd4243b3dedbcf2e975218e2
Kim Johansson <kim@kumiko.se>
2017-11-14 12:06:49 +0100

    Extension method to add Postgresql provider

---
+++ Memstate.Postgresql/MemstateSettingsExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Memstate.Postgresql
+{
+    public static class MemstateSettingsExtensions
+    {
+        public static MemstateSettings UsePostgresqlProvider(this MemstateSettings settings)
+        {
+            settings.StorageProvider = typeof(PostgresqlProvider).AssemblyQualifiedName;
+            
+            return settings;
+        }
+    }
+}

82ed1bc289ee4264e0c1f569adae8e9a3a3de114
Robert Friberg <robert@devrex.se>
2022-12-10 12:53:31 +0100

    update testdependencies.sh

--- test-dependencies.sh
+++ test-dependencies.sh
@@ -12,16 +12,15 @@ fi
 
 if [ "$1" == "stop" -o "$1" == "restart" ]; then
   docker rm -f eventstore
-  docker rm -f postgres
-  docker rm -f mssql2019
+#  docker rm -f postgres
+#  docker rm -f mssql2019
 #  docker-compose down 
 fi
 
 
 if [ "$1" == "start" -o "$1" == "restart" ]; then
-  docker run --name eventstore -d -p 2113:2113 -p 1113:1113 eventstore/eventstore:release-4.1.0
+  docker run --name eventstore -d -p 2113:2113 -p 1113:1113 eventstore/eventstore:22.10.0-buster-slim --insecure --enable-external-tcp
 #  docker run --name postgres -dp5432:5432 -e POSTGRES_PASSWORD='postgres' postgres:9.6.10
 #  docker run --name mssql2019 -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=abc123ABC' -p 1433:1433 -d $mssql_image
 #  docker-compose up -d
 fi
-
